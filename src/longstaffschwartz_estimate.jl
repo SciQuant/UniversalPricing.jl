50536c977f5923f152b2cc6d0028d368da8bde65
Ramiro Vignolo <ramirovignolo@gmail.com>
2021-02-10 19:22:39 -0300

    + moving forward with LSMC

--- src/longstaffschwartz_estimate.jl
+++ src/longstaffschwartz_estimate.jl
@@ -19,8 +19,28 @@ function LongstaffSchwartzExpectation(ExerciseValue, Discount, Regressors, τ, u
 
     @show Tenors
 
-    N = length(Tenors)
-    K = length(u) # number of paths
+    # N = length(Tenors)
+    # K = length(u) # number of paths
+    N, K = size(u)
+
+    #! Dos ideas a implementar:
+    # 1. si tengo que hacer una matriz con 1 indicadores para cada path, lo mejor es hacer
+    #    un vector que me diga en que posicion de la Tenor Structure va el 1.
+
+
+    # 2. Si tengo que hacer una regresion sin considerar los valores out of the money, lo
+    #    que hago es allocar un vector de dimension K una unica vez y luego rellenarlo solo
+    #    con los valores que vamos a usar para la regresion.
+    x = zeros(T, K)
+    y = zeros(T, K)
+
+
+    # CLEs:
+    # 1. Al parecer, el algoritmo para CLEs no elimina los casos con ejercicio out of the
+    #    money porque nunca pasa eso dado que el underlying instrument es un Swap y eso
+    #    siempre tiene valor positivo. Ah no, si puede ser negativo, tire cualquiera.
+    # 2. La ultima fecha no tiene ejercicio, pero en un caso normal si...
+
 
     # exercise dates
     Tₑ = @view Tenors[2:N-1]
@@ -40,8 +60,9 @@ function LongstaffSchwartzExpectation(ExerciseValue, Discount, Regressors, τ, u
 
     # if the exercise value has a closed form solution for its expectation
     for k in 1:K
-        uk = getindex(u, k)
-        for n in 2:N-1
+        # uk = getindex(u, k)
+        uk = getindex(u, :, k)
+        for n in 2:N # for n in 2:N-1
             U[n,k] = ExerciseValue(uk, p, Tenors, n)
         end
     end
@@ -49,7 +70,8 @@ function LongstaffSchwartzExpectation(ExerciseValue, Discount, Regressors, τ, u
     # explanatory variables
     ζ = Matrix{T}(undef, N, K) # en el caso general, esto apunta a un vector
     for k in 1:K
-        uk = getindex(u, k)
+        # uk = getindex(u, k)
+        uk = getindex(u, :, k)
         for n in 2:N-1
             # en el caso general, el usuario entrega una funcion que retorna un vector de
             # dimension `q` con las variables explanatorias dadas las simulaciones del trial
@@ -62,22 +84,54 @@ function LongstaffSchwartzExpectation(ExerciseValue, Discount, Regressors, τ, u
     @. f(x, p) = p[1] + p[2] * x + p[3] * x^2
     param = [0.1, 0.1, 0.1]
 
-    for n in N-1:-1:2
+    for n in N:-1:2 # for n in N-1:-1:2
 
-        if isequal(n, N-1)
+        if isequal(n, N) # isequal(n, N-1)
 
-            # H[N-1,:] has been already set to zero
+            H[n,:] .= zero(T) # it already has been set to zero, but do it anyway (remove above)
             Q[n,:] .= max.(@view(U[n,:]), @view(H[n,:]))
 
         else
 
             # tenemos que construir un interpolador para el hold value por fecha de ejercicio
-            HoldValue = curve_fit(f, ζ[n,:], Discount(p, Tenors[n], Tenors[n+1]) * Q[n+1,:], param; autodiff=:forwarddiff)
+            i = 0
+            fill!(x, zero(T))
+            fill!(y, zero(T))
+            for k in 1:K
+                # podriamos pasar una funcion para ver si estan on the money
+                if U[n,k] > zero(T)
+                    i += 1
+                    x[i] = ζ[n, k]
+                    y[i] = Discount(p, Tenors[n], Tenors[n+1]) * Q[n+1,k]
+                end
+            end
+            x′ = @view x[1:i]
+            y′ = @view y[1:i]
+            @show x′
+            @show y′
+            HoldValue = curve_fit(f, x′, y′, param; autodiff=:forwarddiff)
+            @show HoldValue.param
+
+            # if n == N-2
+            #     plots = plot(x′, x -> f(x, HoldValue.param))
+            # else
+            #     plot!(x′, x -> f(x, HoldValue.param))
+            # end
+
+
+            # sin filtrar
+            # x = @view ζ[n, :]
+            # y = Discount(p, Tenors[n], Tenors[n+1]) * Q[n+1,:]
+            # HoldValue = curve_fit(f, x, y, param; autodiff=:forwarddiff)
 
             for k in 1:K
                 H[n,k] = getindex(f(ζ[n,k], HoldValue.param), 1)
                 Q[n,k] = max(U[n,k], H[n,k])
             end
+
+            @show U[n,:]
+            @show H[n,:]
+            @show Q[n,:]
 
         end
     end
--- test/put.jl
+++ test/put.jl
@@ -52,4 +52,50 @@ end
 
 #! este algoritmo no esta diseñado para tener ejercicio en la ultima fecha, por lo que es de
 #! esperar que no me vaya a dar bien si hago esto! Es para CLEs.
+#! por otro lado, tampoco miramos si la condicion de ejercicio es estrictamente positiva para
+#! los CLEs, porque eso siempre pasa (entro a un Swap, no a put o call q pueden ser cero).
+#! por eso el Andersen no tiene eso.
+#! en el caso de una opcion como esta, tengo que mirar Brigo o LS bibliografia y creo que ahi
+#! se remueven de la regresion los casos donde el valor de ejercicio esta out of the money.
 ζ, U, H, Q, μ, σ = LongstaffSchwartzExpectation(AmericanPutExercise, Discount, Regressors, τ, mc, ds_oop.params)
+
+
+
+
+
+
+
+
+
+function AmericanPutExercise(u, p, Tenors, n)
+    # @unpack x_security = p
+    @unpack K = p
+    # X = remake(x_security, u)
+    t = Tenors[n]
+
+    return max(K - u[n], zero(K))
+end
+
+function Regressors(u, p, Tenors, n)
+    # @unpack x_security = p
+    # X = remake(x_security, u)
+    # t = Tenors[n]
+    return u[n]
+end
+
+function Discount(p, t, T)
+    @unpack r = p
+    return exp(-r * (T - t))
+end
+
+mc = [
+    1.00  1.00  1.00  1.00  1.00  1.00  1.00  1.00
+    1.09  1.16  1.22  0.93  1.11  0.76  0.92  0.88
+    1.08  1.26  1.07  0.97  1.56  0.77  0.84  1.22
+    1.34  1.54  1.03  0.92  1.52  0.9   1.01  1.34
+]
+
+τ = fill(1., 3)
+params = (K=1.10, r = 0.06)
+
+ζ, U, H, Q, μ, σ = LongstaffSchwartzExpectation(AmericanPutExercise, Discount, Regressors, τ, mc, params)
